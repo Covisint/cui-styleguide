6bf417797d338211b0e7069346875fda0d643cfb
Shane Ardell <shane.ardell@thirdwavellc.com>
2015-11-16 18:36:10 +0100

    adds desktop menu

--- styleguide/public/scripts/cui-scripts.js
+++ styleguide/public/scripts/cui-scripts.js
@@ -2,3 +2,10 @@ function toggle_visibility(Id) {
    var e = document.getElementById(Id);
    e.style.opacity = ((e.style.opacity!='0') ? '0' : '1');
 }
+
+function toggle_desktop_nav() {
+   var e = document.querySelector('.desktop-menu');
+   var wrapper = document.querySelector('.cui-nav__content-wrapper');
+   e.classList.toggle('desktop-menu--collapse');
+   wrapper.classList.toggle('desktop-menu--collapse');
+}
