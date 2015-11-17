022fca46b551e329675e5dcf2b7b0377fa89ce38
Shane Ardell <shane.ardell@thirdwavellc.com>
2015-11-17 19:35:03 +0100

    adds Snap mobile menu markup & js

--- source/scripts/cui-scripts.js
+++ source/scripts/cui-scripts.js
@@ -3,9 +3,42 @@ function toggle_visibility(Id) {
    e.style.opacity = ((e.style.opacity!='0') ? '0' : '1');
 }
 
-function toggle_desktop_nav() {
-   var e = document.querySelector('.desktop-menu');
-   var wrapper = document.querySelector('.cui-nav__content-wrapper');
-   e.classList.toggle('desktop-menu--collapse');
-   wrapper.classList.toggle('desktop-menu--collapse');
-}
+// Desktop Navigation Functions
+
+var Nav = (function () {
+
+  var cache = new Array();
+  cache.push(document.querySelector('.desktop-menu'));
+  cache.push(document.querySelector('.cui-nav__content-wrapper'));
+  cache.push(document.querySelector('.menu-toggle'));
+
+  function toggleDesktopNav() {
+    cache[0].classList.toggle('desktop-menu--collapse');
+    cache[1].classList.toggle('desktop-menu--collapse');
+  };
+
+  return {
+    navToggle: toggleDesktopNav
+  };
+
+}) ();
+
+// Snap.js Mobile Navigation
+
+var snapMenu = new Snap({
+    element: document.querySelector('.snap-content'),
+    disable: 'right'
+});
+
+var menuToggle = document.querySelector('.menu-toggle')
+
+menuToggle.addEventListener('click', function() {
+
+    if (snapMenu.state().state == "left") {
+        snapMenu.close();
+    } else {
+        snapMenu.open('left');
+    }
+
+});
+
