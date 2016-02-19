d9fa67be26827ba89d35f6fb29c62ddc2f9152e2
Shane Ardell <shane.ardell@thirdwavellc.com>
2016-02-19 19:57:35 +0100

    cleans up expandable js

--- source/scripts/cui-scripts.js
+++ source/scripts/cui-scripts.js
@@ -24,23 +24,6 @@ var TopNav = (function () {
 
 }) ();
 
-
-
-// var Expandable = (function () {
-
-//   var e = document.querySelectorAll('.cui-expandable__title');
-
-//   for (var i = 0; i < e.length; i++) {
-//     e[i].addEventListener('click', function() {expandableToggle(this);} );
-//   }
-
-//   function expandableToggle(elem) {
-//     var parent = elem.parentNode;
-//     parent.classList.toggle('expanded');
-//   };
-
-// }) ();
-
 // Desktop Navigation Functions
 
 var Nav = (function () {
@@ -102,8 +85,46 @@ var Tabs = (function () {
       p[i].className = 'cui-tabs__tab-pane';
     }
     elem.className = 'cui-tabs__tab cui-tabs__tab--active';
-    pane.className = 'cui-tabs__tab-pane cui-tabs__tab-pane--active'
+    pane.className = 'cui-tabs__tab-pane cui-tabs__tab-pane--active';
   };
 
 }) ();
 
+$(document).ready(function(){
+
+  (function() {
+
+    var Expandable = {
+
+      init: function() {
+        this.cacheDOM();
+        this.closeOnLoad();
+        this.bindWatch();
+      },
+
+      cacheDOM: function() {
+        this.$el = $(".cui-expandable__title");
+        this.$wrapper = this.$el.parent()
+      },
+
+      closeOnLoad: function() {
+        this.$wrapper.each(function() {
+          if (!$(this).hasClass("expanded")) {
+            $(this).children(".cui-expandable__body").css('display','none');
+          }
+        });
+      },
+
+      bindWatch: function() {
+        this.$el.on('click', this.toggleExpandable);
+      },
+
+      toggleExpandable: function() {
+        $(this).parent().toggleClass("expanded");
+        $(this).siblings(".cui-expandable__body").animate({'height':'toggle'},300,'linear');
+      }
+
+    };
+    Expandable.init();
+  }) ()
+});
